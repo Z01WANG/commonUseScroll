d0edc465faa52259118106f05e3f4d7ba0f75365
Zoe <Lezoy1699@Gmail.com>
2024-03-12 10:53:38 +0800

    bug fixed

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
     // setActiveTab(headerHeight);
     
     // 點擊tab後滾動到相對應位置   
-    $(".anchor-btn").on("click", function (event) {
+    $(".anchor_btn").on("click", function (event) {
       event.preventDefault();
       let marginSpace = 32;
       let targetId = $(this).data("target");
@@ -33,6 +33,7 @@ $(document).ready(function () {
   
       // scroll function
       $(window).scroll(function () {
+        console.log("???")
         setActiveTab(headerHeight);
     });
   
@@ -43,19 +44,20 @@ $(document).ready(function () {
   });
   
   function adjustScrollPositionForHash() {
-    if(window.location.hash) {
+    if (window.location.hash) {
       let headerHeight = $(".header-top").outerHeight(); // Get the updated header height
       let marginSpace = 32; // Margin space
       let targetId = window.location.hash.slice(1); // Remove '#' from the hash
       let targetSection = $("#" + targetId);
-      
-      if(targetSection.length) {
+  
+      if (targetSection.length) {
         let targetOffset = targetSection.offset().top - headerHeight - marginSpace;
         // Use setTimeout to ensure any last-minute layout adjustments are accounted for
-        setTimeout(function() {
+        setTimeout(function () {
           $("html, body").animate({ scrollTop: targetOffset }, 200, 'swing');
         }, 100);
       }
+      setActiveTab(headerHeight);
     }
   }
   
@@ -66,24 +68,26 @@ $(document).ready(function () {
   
   // 活動頁導覽列active設定
   function setActiveTab(headerHeight) {
+    
     let isActiveSet = false; // 重要
     
-    $(".sub-section-box").each(function () {
+    $(".anchor_section").each(function () {
       let scrollPosition = $(window).scrollTop() + headerHeight;
       let sectionTop = $(this).offset().top - headerHeight;
       let sectionId = $(this).attr("id");
       let sectionBottom = sectionTop + $(this).outerHeight(); // 超過section就移除
 
       if (scrollPosition >= sectionTop - 80 && scrollPosition < sectionBottom) {
-        $(".anchor-btn").removeClass("active");
-        let activeTab = $('.anchor-btn[data-target="' + sectionId + '"]');
+        $(".anchor_btn").removeClass("active");
+        let activeTab = $('.anchor_btn[data-target="' + sectionId + '"]');
+        console.log('activetab',activeTab);
         activeTab.addClass("active");
         scrollToTab(activeTab);
         isActiveSet = true;
       }
     });
     if (!isActiveSet) {
-      $(".anchor-btn").removeClass("active");
+      $(".anchor_btn").removeClass("active");
     }
   }
   
